342585118b50010cc53ad22993c32833c69ae01e
Alcino José <steveneizy1294@gmail.com>
2025-03-10 18:00:49 -0300

    feat: Add initial pages

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,11 @@
+interface Params{
+    id: string;
+}
+
+
+const ProductDetailsPage = ({params: {id}}: {params: Params}) => {
+    return ( <h1>Product ID: {id} </h1> );
+}
+
+
+export default ProductDetailsPage;
