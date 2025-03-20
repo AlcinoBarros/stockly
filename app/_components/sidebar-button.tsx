06fe21381622340e9581d4886caa95f9cec44467
Alcino José <steveneizy1294@gmail.com>
2025-03-20 17:12:48 -0300

    chore: adjust in settings

--- app/_components/sidebar-button.tsx
+++ app/_components/sidebar-button.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import Link from "next/link";
+
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface SidebarButtonProps{
     children: React.ReactNode;
@@ -13,15 +14,15 @@ const SidebarButton = ({href, children}: SidebarButtonProps) => {
     const pathname = usePathname();
 
     return (
-        <Button variant={pathname == `${href}` ? "secondary" : "ghost"} 
-        className="justify-start gap-2" 
-        asChild>
-                    <Link href={href}>
-                        {children}
-                    </Link>
-                </Button>
+        <Button 
+            variant={pathname == `${href}` ? "secondary" : "ghost"} 
+            className="justify-start gap-2" 
+            asChild
+        >
+            <Link href={href}>{children}</Link>
+        </Button>
     );
-}
+};
 
 
-export default SidebarButton
+export default SidebarButton;
--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import { LayoutGridIcon, PackageIcon, ShoppingBasketIcon } from "lucide-react";
 
